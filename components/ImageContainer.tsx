07b2ad52ef0a1dae953d8a91060b33a6dba31f3a
Mahdiyar Delavari <MahdiyarDelavari@gmail.com>
2025-08-14 20:55:07 +0330

    update ImageContainer

--- components/ImageContainer.tsx
+++ components/ImageContainer.tsx
@@ -1,24 +1,37 @@
 import type { Photo } from "@/models/Images";
 import Image from "next/image";
-
+import Link from "next/link";
 type PropsType = {
 	photo: Photo;
 };
 
 const ImageContainer = ({ photo }: PropsType) => {
+	const widthHeightRatio = photo.height / photo.width;
+	const galleryHeight = Math.ceil(250 * widthHeightRatio);
+	const photoSpans = Math.ceil(galleryHeight / 10) + 1;
 	return (
 		<div
-			className="h-64 bg-gray-200 rounded-3xl relative overflow-hidden"
+			className="w-[250px] justify-self-center"
+			style={{ gridRow: `span ${photoSpans}` }}
 		>
-			<Image
-				src={photo.src.large}
-				alt={photo.alt}
-				fill={true}
-                placeholder="blur"
-                blurDataURL={photo.blurredDataUrl}
-                className="object-cover hover:opacity-75"
-				sizes="(min-width: 1280px) 278px, (min-width: 1040px) calc(12.73vw + 118px), (min-width: 800px) 33.18vw, (min-width: 540px) 50vw, calc(100vw - 16px)"
-			/>
+			<Link
+				href={photo.url}
+				target="_blank"
+				className="grid place-content-around"
+			>
+				<div className="oveflow-hidden">
+					<Image
+						src={photo.src.large}
+						alt={photo.alt}
+                        width={photo.width}
+                        height={photo.height}
+						placeholder="blur"
+						blurDataURL={photo.blurredDataUrl}
+						className="hover:opacity-75 rounded-2xl"
+						sizes="250px"
+					/>
+				</div>
+			</Link>
 		</div>
 	);
 };
