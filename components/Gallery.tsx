e912a94574404c37acd9e495d7da03682e3900b3
Mahdiyar Delavari <MahdiyarDelavari@gmail.com>
2025-08-14 19:39:03 +0330

    Add Blur

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -1,6 +1,7 @@
 import fetchImages from "@/lib/fetchImages";
 import type { ImagesResults } from "@/models/Images";
 import ImageContainer from "./ImageContainer";
+import addBlurredDataUrls from "@/lib/getBase64";
 
 const Gallery = async () => {
 	const url = "https://api.pexels.com/v1/curated";
@@ -9,10 +10,12 @@ const Gallery = async () => {
 
 	if (!images) {
 		return <h2 className="m-4 text-2xl font-bold">No Images Found</h2>;
-	}
+    }
+    
+    const photoWithBlur = await addBlurredDataUrls(images)
 	return (
 		<section className="px-2 my-3 grid gap-2 grid-cols-gallery">
-			{images.photos.map((photo) => (
+			{photoWithBlur.map((photo) => (
 				<ImageContainer key={photo.id} photo={photo} />
 			))}
 		</section>
--- components/ImageContainer.tsx
+++ components/ImageContainer.tsx
@@ -8,14 +8,15 @@ type PropsType = {
 const ImageContainer = ({ photo }: PropsType) => {
 	return (
 		<div
-			key={photo.id}
 			className="h-64 bg-gray-200 rounded-3xl relative overflow-hidden"
 		>
 			<Image
 				src={photo.src.large}
 				alt={photo.alt}
 				fill={true}
-				className="object-cover hover:opacity-75"
+                placeholder="blur"
+                blurDataURL={photo.blurredDataUrl}
+                className="object-cover hover:opacity-75"
 				sizes="(min-width: 1280px) 278px, (min-width: 1040px) calc(12.73vw + 118px), (min-width: 800px) 33.18vw, (min-width: 540px) 50vw, calc(100vw - 16px)"
 			/>
 		</div>
